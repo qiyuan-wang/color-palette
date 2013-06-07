4d2729af3cdfd40e68638798eac04aa0f33c5f08
zephyrisalive <zephyrisalive@gmail.com>
2013-06-07 08:28:48 +0800

    project init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Color::Application.config.secret_token = '8f0e5ec1c6a9c9c849d9967c7fb22d809124bd24b00c875f7536024eaf323d3351b4971136babfd8a180ef639ea40460a8a63e96f1c6b39a66b852df13164cc2'
